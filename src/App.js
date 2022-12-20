b757b8b0347ff6571404524cda244fabcc0d93f3
benjamin farmer <viroben2001@gmail.com>
2022-12-19 23:50:23 -0800

    finished code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+function App(){
+    let [colors, setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+    const addColor = (newColor) => {
+      setColors([...colors, newColor])
+  }
+    return (
+      <div className="App">
+        {colors.map(( color, i) => 
+        <ColorBlock color={color} key={i} />)}
+        <ColorForm addColor={addColor}/> 
+      </div>
+    )
 }
+
+
+  
+
+
+
+
+  
+
+
 
 export default App;
